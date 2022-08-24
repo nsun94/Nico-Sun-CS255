62b93207b562ee909228d12c67d350c98e553a9d
nsun94 <56122707+nsun94@users.noreply.github.com>
2022-08-24 11:58:43 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/**********************************************************
+* Hello.Java
+*************************************************************
+* Nico Sun
+* 8/24/22
+* CMSC255 004
+**********************************************************/
+
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Nico Sun>");
+		System.out.println("<Hello Java>");
+		System.out.println("<Basic coding assignment to familiarize students with java>");
+		System.out.println("<8/24/22>");
+		System.out.println("<CMSC255 004>");
+		System.out.println();
+
+	}
+}
